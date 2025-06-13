80d27a1613f2fe458a0568d83ec1c9adc7ffbbca
Samuel Mulia <samuel@192.168.1.10>
2025-06-14 05:44:37 +0700

    Add type declarations for SpeechRecognition

---
+++ src/types/globals.d.ts
@@ -0,0 +1,9 @@
+// src/types/globals.d.ts
+export {};
+
+declare global {
+  interface Window {
+    SpeechRecognition: typeof SpeechRecognition;
+    webkitSpeechRecognition: typeof SpeechRecognition;
+  }
+}
